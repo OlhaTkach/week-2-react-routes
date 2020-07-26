39a50f76ea3037c6e23cdc0e16597593797d6612
OlhaTkach <olya.weaver@gmail.com>
2020-07-26 18:19:19 +0300

    fixed getiing profile id

--- client/components/profile.js
+++ client/components/profile.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { useParams, Link } from 'react-router-dom'
 
 const Profile = () => {
-  const { username } = useParams()
+  const { user } = useParams()
   return (
     <div>
       <div id="title">Profile</div> 
-      <div id="username">{username}</div> 
+      <div id="username">{user}</div> 
       <Link to="/dashboard"> Go To Root</Link>
       <Link to="/dashboard/main"> Go To Main </Link>
     </div>
