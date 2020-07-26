d27da397b22bdb9656e458971fd44adfa8f3079a
OlhaTkach <olya.weaver@gmail.com>
2020-07-26 19:13:31 +0300

    fixed dashboard title

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom'
 
 const Dashboard = () => {
   return (
-    <div id="title">Dashboard
+    <div>
+      <div id="title">Dashboard</div>
       <Link to="/dashboard/profile/d71e4430-5383-4865-9f7c-5edfdbccb2da">Go To Profile</Link>
       <Link to="/dashboard">Go To Main</Link>
     </div>
