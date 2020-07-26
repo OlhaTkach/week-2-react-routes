b6a50deb34df1a7e8fb35870d155c53c980b4380
OlhaTkach <olya.weaver@gmail.com>
2020-07-26 19:56:51 +0300

    fix main link

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -6,7 +6,7 @@ const Dashboard = () => {
     <div>
       <div id="title">Dashboard</div>
       <Link to="/dashboard/profile/d71e4430-5383-4865-9f7c-5edfdbccb2da">Go To Profile</Link>
-      <Link to="/dashboard">Go To Main</Link>
+      <Link to="/dashboard/main">Go To Main</Link>
     </div>
   )
 }
