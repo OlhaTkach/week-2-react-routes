1abc8adbbc5c314ffed56d11cc77c2b4a579fc75
OlhaTkach <olya.weaver@gmail.com>
2020-07-26 19:20:11 +0300

    fixed main title

--- client/components/main-page.js
+++ client/components/main-page.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom'
 
 const MainPage = () => {
   return (
-    <div id="title">Main
+    <div>
+      <div id="title">Main</div>
       <Link to="/dashboard/profile/d71e4430-5383-4865-9f7c-5edfdbccb2da">Go To Profile</Link>
       <Link to="/dashboard">Go To Root</Link>
     </div>    
